7b95609ce30e0c10e4d1a01567eeae59dec73242
Julian Currie <julian.currie@gmail.com>
2016-10-24 03:51:43 -0700

    add lat/long to API call using Geocoder class and an ES6 template string.

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import {
     Text,
     StyleSheet
 } from 'react-native';
+import Geocoder from 'react-native-geocoder';
 
-//Don't store your tokens this way in a normal production app :)
+//Normally wouldn't store my token this way if this were a production app :)
 const API_TOKEN = 'Bearer 4H6ELX745M6BUM6W7A4C';
 const SEARCH_URL = 'https://www.eventbriteapi.com/v3/events/search/';
 
@@ -21,20 +22,24 @@ export default class Main extends Component {
     }
 
     searchEvents(category, city) {
-        const FETCH_URL = SEARCH_URL + '?q=' + category + '&location.address=' + city;
+        Geocoder.geocodeAddress(city).then(geoCodeResponse => {
+            console.log('res', geoCodeResponse);
 
-         console.log(FETCH_URL);
+            let position = geoCodeResponse[0].position;
+            let locationString = `&location.latitude=${position.lat}&location.longitude=${position.lng}`;
+            let FETCH_URL = `${SEARCH_URL}?q=${category}${locationString}`;
 
-        fetch(FETCH_URL, {
-            method: 'GET',
-            headers: {
-                'Authorization': API_TOKEN
-            }
-        })
-        .then((response) => response.json())
-        .then((responseJSON) => {
-            console.log(responseJSON);
-        });
+            fetch(FETCH_URL, {
+                method: 'GET',
+                headers: {
+                    'Authorization': API_TOKEN
+                }
+            })
+            .then((response) => response.json())
+            .then((responseJSON) => {
+                console.log(responseJSON);
+            });
+        }).catch((err) => console.log('Error', err));
     }
 
     render() {
