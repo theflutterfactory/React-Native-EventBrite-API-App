7aba8d01f39ddaaf69d3c8639d4ab968d4665406
Julian Currie <julian.currie@gmail.com>
2016-10-23 21:01:56 -0700

    initial commit

---
+++ index.android.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+import Main from './src/main';
+
+AppRegistry.registerComponent('ReactNativeEventBriteAPIExample', () => Main);
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+    View
+} from 'react-native';
+
+export default class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <View>
+            </View>
+        );
+    }
+}
