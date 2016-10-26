e1f83383a486031932403c97247197b8d636faa7
Julian Currie <julian.currie@gmail.com>
2016-10-25 19:16:37 -0700

    added header views for navigation, style updates

--- src/components/event_detail.js
+++ src/components/event_detail.js
@@ -2,18 +2,29 @@ import React, { Component } from 'react';
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    TouchableOpacity
 } from 'react-native';
 
 export default class EventDetail extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>Event Detail</Text>
-                <Text>{this.props.title}</Text>
-                <Text>{this.props.description}</Text>
-                <Text>{this.props.img}</Text>
-                <Text>{this.props.url}</Text>
+                <View style={styles.header}>
+                    <TouchableOpacity>
+                        <Text style={styles.link}>Back</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity >
+                        <Text style={styles.link}>Full Details</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.body}>
+                    <Text>Event Detail</Text>
+                    <Text>{this.props.title}</Text>
+                    <Text>{this.props.description}</Text>
+                    <Text>{this.props.img}</Text>
+                    <Text>{this.props.url}</Text>
+                </View>
             </View>
         );
     }
@@ -21,8 +32,18 @@ export default class EventDetail extends Component {
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1
+    },
+    header: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        flexDirection: 'row',
+        padding: 20,
+        justifyContent: 'space-between'
+    },
+    body: {
+        flex: 19
+    },
+    link: {
+        color: 'blue'
     }
 });
